3df3268c47a705e272fb43b51fc0cc7f559ab98b
azh <2550364626@qq.com>
2022-07-01 22:43:20 +0800

    线程间通信:wait/notify

---
+++ threadCommunication/src/main/java/waitnotifycounsumerproducerthread/WaitNotifyProducerConsumer_1.java
@@ -0,0 +1,37 @@
+package waitnotifycounsumerproducerthread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class WaitNotifyProducerConsumer_1 {
+    private static final Object lock = new Object();
+
+    private static AtomicInteger count = new AtomicInteger();
+
+    public static void main(String[] args) {
+        Thread producer = new Thread(new Runnable() {
+            public void run() {
+                synchronized(lock){
+                    System.out.println("生产者开始生产数字 count = " + count.incrementAndGet());
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        // doNothing
+                    }
+                }
+            }
+        }, "producer");
+
+
+        Thread consumer = new Thread(new Runnable() {
+            public void run() {
+                synchronized (lock){
+                    System.out.println("消费者开始消费数字 count = " + count.decrementAndGet());
+                    lock.notify();
+                }
+            }
+        }, "consumer");
+
+        producer.start();
+        consumer.start();
+    }
+}
