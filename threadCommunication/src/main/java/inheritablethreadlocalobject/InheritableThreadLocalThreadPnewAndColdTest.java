3ef7a14211458ed7154e95b13543cb03ca30cdc4
azh <2550364626@qq.com>
2022-07-16 09:14:01 +0800

    ThreadLocal

---
+++ threadCommunication/src/main/java/inheritablethreadlocalobject/InheritableThreadLocalThreadPnewAndColdTest.java
@@ -0,0 +1,25 @@
+package inheritablethreadlocalobject;
+
+public class InheritableThreadLocalThreadPnewAndColdTest {
+    public static void main(String[] args) {
+        try{
+            for(int i=0; i<10; i++){
+                if(InheritableThreadLocalDemo.inheritableThreadLocal.get() == null){
+                    InheritableThreadLocalDemo.inheritableThreadLocal.set("此值是 main 线程 放入的!");
+                }
+                System.out.println("           在 main 线程中取值=" + InheritableThreadLocalDemo.inheritableThreadLocal.get());
+                Thread.sleep(100);
+            }
+            InheritableThreadLocalThread threadLocalThread = new InheritableThreadLocalThread();
+            Thread t1 = new Thread(threadLocalThread);
+            t1.start();
+            Thread.sleep(300);
+            InheritableThreadLocalDemo.inheritableThreadLocal.set("此值是 main 线程 new 放入的");
+            System.out.println("               在 main 线程中取值=" + InheritableThreadLocalDemo.inheritableThreadLocal.get());
+            
+            
+        }catch (InterruptedException e){
+            // doNothing
+        }
+    }
+}
