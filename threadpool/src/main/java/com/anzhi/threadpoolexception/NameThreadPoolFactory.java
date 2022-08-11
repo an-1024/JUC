5b7c8fc0229f08351231f305d62383dfaaafe479
azh <2550364626@qq.com>
2022-08-11 16:41:40 +0800

    线程池 submit 异常处理

---
+++ threadpool/src/main/java/com/anzhi/threadpoolexception/NameThreadPoolFactory.java
@@ -0,0 +1,50 @@
+package com.anzhi.threadpoolexception;
+
+import java.util.Objects;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class NameThreadPoolFactory extends ThreadPoolExecutor {
+
+    public NameThreadPoolFactory(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    }
+
+    public NameThreadPoolFactory(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+    public NameThreadPoolFactory(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+    }
+
+    public NameThreadPoolFactory(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+    }
+
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+        super.afterExecute(r, t);
+        if (t == null && r instanceof Future<?>) {
+            try {
+                //get这里会首先检查任务的状态，然后将上面的异常包装成ExecutionException
+                Object result = ((Future<?>) r).get();
+            } catch (CancellationException ce) {
+                t = ce;
+            } catch (ExecutionException ee) {
+                t = ee.getCause();
+                System.out.println("---------------" + t.getMessage());
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt(); // ignore/reset
+            }finally {
+                this.shutdown();
+            }
+        }
+    }
+}
--- threadpool/src/main/java/com/anzhi/threadpoolexception/ThreadPoolExceptionDemo.java
+++ threadpool/src/main/java/com/anzhi/threadpoolexception/ThreadPoolExceptionDemo.java
@@ -1,5 +1,6 @@
 package com.anzhi.threadpoolexception;
 
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -8,7 +9,7 @@ import java.util.concurrent.TimeUnit;
  *  验证线程池提交任务异常不会抛出
  */
 public class ThreadPoolExceptionDemo {
-    private static final ThreadPoolExecutor EXCEPTION_TEST_EXECUTOR = new ThreadPoolExecutor(20, 20, 0, TimeUnit.SECONDS, new LinkedBlockingQueue<>(100000));
+    private static final ThreadPoolExecutor EXCEPTION_TEST_EXECUTOR = new NameThreadPoolFactory(20, 20, 0, TimeUnit.SECONDS, new LinkedBlockingQueue<>(100000));
 
     public static void main(String[] args) {
         try {
@@ -16,7 +17,7 @@ public class ThreadPoolExceptionDemo {
                 int i = 1 / 0;
             });
         }catch (Exception e){
-            System.out.println("执行异常");
+            System.out.println("--------------执行异常");
         }
     }
 }
