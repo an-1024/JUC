0b742e643656986b8e5417550ccfa81377d00ed9
azh <2550364626@qq.com>
2023-02-02 00:28:19 +0800

    ForkJoin

--- threadpool/src/main/java/com/anzhi/forkjoin/ForkJoinPoolDemo.java
+++ threadpool/src/main/java/com/anzhi/forkjoin/ForkJoinPoolDemo.java
@@ -4,6 +4,12 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 
 public class ForkJoinPoolDemo {
+
+    volatile long ctl;                   // main pool control
+    volatile static int runState=0;               // lockable status
+    private static final int  RSLOCK     = 1;
+
+
     public static void main(String[] args) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         forkJoinPool.submit(()-> System.out.println("ForkJoinPool Demo"));
