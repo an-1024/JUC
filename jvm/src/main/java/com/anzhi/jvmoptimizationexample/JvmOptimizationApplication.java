ed97dca4a5457118a6b3fa394b741e8a899895c6
azh <2550364626@qq.com>
2023-02-17 14:10:52 +0800

    GC 实战

---
+++ jvm/src/main/java/com/anzhi/jvmoptimizationexample/JvmOptimizationApplication.java
@@ -0,0 +1,21 @@
+package com.anzhi.jvmoptimizationexample;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class JvmOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmOptimizationApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
